f557bb24152ca9528e562bb92666973858dbe2a0
Svetlana <111175@rambler.ru>
2024-09-01 10:51:23 +0300

    add string[] GetArrayThreeSimvol

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,3 +1,29 @@
-﻿Console.Write("Введите строку, разделенную запятыми:");
+﻿string[] GetArrayThreeSimvol(string[] array)
+{
+    int countLength = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            countLength++;
+        }
+        
+    }
+    string[] arrayThreeSimvol = new string[countLength];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+        
+    }
+    return arrayThreeSimvol;
+}
+
+Console.Write("Введите строку, разделенную запятыми:");
 string? arrayElement = Console.ReadLine();
 string[] arraySimvol = arrayElement.Split(',');
